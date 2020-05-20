14285b2b6d948642b7b938cffd2489551a41057e
moonelysian <dandyblack@naver.com>
2020-05-20 13:35:10 +0900

    특정 히스토리로 돌리면 그 이후 히스토리는 삭제하도록 수정

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -79,7 +79,8 @@ class Game extends React.Component {
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0
+      xIsNext: (step % 2) === 0,
+      history: this.state.history.slice(0, step+1)
     });
   }
 
